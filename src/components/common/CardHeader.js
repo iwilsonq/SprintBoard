d5ad45409005f4643d9a3c8a17b546b5075ddcb5
iwilsonq <iwilsonq@gmail.com>
2017-02-24 01:51:01 -0800

    attached colors to rules, random card color gen

--- src/components/common/CardHeader.js
+++ src/components/common/CardHeader.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const CardHeader = props => {
+  const colors = ['red', 'tan', 'steel', 'success', 'fail'];
+  const randomIndex = Math.floor(Math.random() * colors.length);
+
   return (
-    <div className="card-header-wrapper">
-      <div className="card-header">
-        <h2 className="lightfont">A header</h2>
-        <p className="lightfont">A description of this card</p>
-      </div>
+    <div className={`card-header ${colors[randomIndex]}`}>
+      <h2 className="lightfont">A header</h2>
+      <p className="lightfont">A description of this card</p>
     </div>
   );
 };
